8f58b5c69858fb7d1ae53c552ddcc5aa41a1ae69
SJK146 <66105006+SJK146@users.noreply.github.com>
2023-10-26 16:45:46 -0400

    Create API_Calls.py

---
+++ Backend/SQL_stuff/API_Calls.py
@@ -0,0 +1,14 @@
+https://www.alphavantage.co/    (5 API requests per minute and 100 requests per day.)
+    ALOT OF TESTS
+    SMA, EMA, WMA, DEMA, TEMA, MACDEXT, RSI.........
+
+https://polygon.io/docs/stocks/get_v1_indicators_sma__stockticker  (5 API Calls / Minute)
+    MISC pricing informaiton
+    SMA, EMA, MACD, RSI 
+
+Anything on (web scraping):
+https://finance.yahoo.com/quote/NVDA/key-statistics?p=NVDA
+    for example, EPS, PE, Open close, Ranges, avg volume
+
+Sentiment analysis (is it good bad or ugly)
+https://www.marketaux.com/documentation
