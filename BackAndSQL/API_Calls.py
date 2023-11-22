3ad2eb283a95f2c566ea3524f9428dfe6fba40f7
SJK146 <66105006+SJK146@users.noreply.github.com>
2023-11-21 19:49:14 -0500

    Update API_Calls.py

--- BackAndSQL/API_Calls.py
+++ BackAndSQL/API_Calls.py
@@ -1,8 +1,7 @@
 #Kyle
-#https://docs.google.com/spreadsheets/d/1glnLrH92hvDENHy2s_REYXJ2cGgpRx10YTlDqKx7xmU/edit?usp=sharing
+#add  BBANDS, STOCH
 import requests
 import json
-
 
 alphaAPIKey = "FV940XIF9BKNU8P5"  
 
