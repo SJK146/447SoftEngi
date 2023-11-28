ef69911c44e84d4a5b7823beaf1534e94dc294dc
vboxuser <vboxuser@411Project.myguest.virtualbox.org>
2023-11-28 15:13:57 -0500

    added some comments and formatting

--- BackAndSQL/main.py
+++ BackAndSQL/main.py
@@ -2,6 +2,8 @@
 #then compares the bd value to the api value 
 # then calls email thingy 
 
+import API_Calls
+
 from flask import Flask
 from data_processor import run_data_processor
 # from api_handler import handle_api_requests
@@ -12,6 +14,10 @@ from ....project.models import db, User, Study, Tests
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
+#Timer waiting 
+import datetime 
+import time
 
 ######
 #we need an email for the email thing to work, so someone needs to create it
@@ -30,6 +36,25 @@ if __name__ == '__main__':
     run_data_processor(db)
     # handle_api_requests()
 
+    target_time = datetime.time(7, 30)#7:30AM
+    while True:
+        current_time = datetime.datetime.now().time()
+        if current_time >= target_time:
+            break
+        time.sleep(45)#sleeps for 45 seconds to put it within the exact minute to run 
+    print(f"Starting db comparisons")
+    sleep(15)
+
+
+#SMA, EMA, RSI, MACDEXT, BBANDS, STOCH, MACD
+#list of comparisons, compare return value to db value 
+#API_Calls.alpha_SMA(symbol, interval, time_period, series_type)
+#API_Calls.alpha_EMA(symbol, interval, time_period, series_type)
+#API_Calls.alpha_macdcext(symbol, interval, series_type)
+#API_Calls.alpha_rsi(symbol, interval, time_period, series_type)
+#API_Calls.alpha_bands(symbol, interval, time_period, series_type)
+#API_Calls.alpha_stoch(symbol, interval)
+#API_Calls.poly_macd(symbol, timespan)
 
 
 
@@ -39,8 +64,10 @@ def send_email(recipient_email):
     subject = "Stock Trigger Notification"
     body = "Hello, your stock has hit its trigger!"
     message = MIMEMultipart()
+
     message["From"] = "your_email@gmail.com"
     password = "your_email_password"
+
     message["To"] = recipient_email
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
